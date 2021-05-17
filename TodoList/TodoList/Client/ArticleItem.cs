c0f4e378cbc7574212592974abf55f1caab23053
정연 이 <jylee@langcode.io>
2021-05-17 18:05:59 +0900

    Article

---
+++ TodoList/TodoList/Client/ArticleItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Client
+{
+    public class ArticleItem
+    {
+        public string Title { get; set; }
+        public string Article { get; set; }
+    }
+}
