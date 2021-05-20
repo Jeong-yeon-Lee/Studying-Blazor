edf60b1b850064f1190ec615ac2a62ec65972b99
정연 이 <jylee@langcode.io>
2021-05-20 21:58:11 +0900

    simsim chat

---
+++ Chat/Chat/Client/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Client
+{
+    public class Chatt
+    {
+        public string Mymessage { get; set; }
+        public bool IsSent { get; set; }
+        
+    }
+}
